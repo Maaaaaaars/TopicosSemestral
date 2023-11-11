9a94ca54a2c35d20d5ee7aa0da0db0be9a98fd51
Bruno Arce R <brunoarce0@gmail.com>
2023-11-10 22:52:37 -0300

    Modularized functions
    Ahora el handler se encarga de retornar los valores maximos para crear el space

--- Test1.py
+++ Test1.py
@@ -1,4 +1,4 @@
-from reading import *
+from bundleHandler import *
 import math
 
 class Space:
@@ -20,26 +20,12 @@ class Space:
 
 def main():
     data = read_bundlesdata('sub7.bundlesdata')
-    max_x = 0
-    max_y = 0
-    max_z = 0
-    for trajectory in data:
-        for coord in trajectory:
-            max_x = max(max_x, coord[0])
-            max_y = max(max_y, coord[1])
-            max_z = max(max_z, coord[2])
 
-    print(f'Max X: {max_x}, Max Y: {max_y}, Max Z: {max_z}')
+    space = Space(*maxValues(data), 10, 10)
 
-    ceil_x = math.ceil(max_x / 10.0) * 10
-    ceil_y = math.ceil(max_y / 10.0) * 10
-    ceil_z = math.ceil(max_z / 10.0) * 10
+    print(space.get_coord('x'))
+   
 
-    print(f'Ceil X: {ceil_x}, Ceil Y: {ceil_y}, Ceil Z: {ceil_z}')
-
-    space = Space(ceil_x, ceil_y, ceil_z, 10, 10)
-
-    print(f'X: {space.get_coord("x")}, Y: {space.get_coord("y")}, Z: {space.get_coord("z")}, D: {space.D}, R: {space.r}')
 
 main()
 
---
+++ bundleHandler.py
@@ -0,0 +1,26 @@
+import numpy as np
+import math
+
+#Notas: El primer iterador va hasta  B-1
+#El segundo iterador va hasta 21
+#El tercer iterador va hasta 3
+
+def read_bundlesdata(filename):
+    A = np.fromfile(filename, dtype=np.float32)
+    B = A.reshape(-1, 64)[:, 1:].reshape(-1, 21, 3)
+    return B
+
+def maxValues(data):
+    max_x = 0
+    max_y = 0
+    max_z = 0
+    for trajectory in data:
+        for coord in trajectory:
+            max_x = max(max_x, coord[0])
+            max_y = max(max_y, coord[1])
+            max_z = max(max_z, coord[2])
+    
+    max_x = math.ceil(max_x / 10.0) * 10
+    max_y = math.ceil(max_y / 10.0) * 10
+    max_z = math.ceil(max_z / 10.0) * 10
+    return max_x, max_y, max_z
