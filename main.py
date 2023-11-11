633e8c797e12bea238cdcecdc5349de0fa58f047
Bruno Arce R <brunoarce0@gmail.com>
2023-11-11 15:28:28 -0300

    Modularized space
    Ahora space está conentido en un solo archivo

---
+++ main.py
@@ -0,0 +1,17 @@
+from space import *
+
+
+def main():
+    data = read_bundlesdata('sub7.bundlesdata')
+
+    space = Space(*maxValues(data), 10, 10)
+
+    print(space.getCoord('x'))
+
+    C = space.getCenters()
+    print(C)
+
+
+
+
+main()
